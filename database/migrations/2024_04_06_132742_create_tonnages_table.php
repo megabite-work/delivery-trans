5fd2901c78794a1537a6b276f634920894566afd
Yuri Zanin <y@zanin.me>
2024-06-16 23:40:24 +0300

    Fix Migrations

--- database/migrations/2024_04_06_132742_create_tonnages_table.php
+++ database/migrations/2024_04_06_132742_create_tonnages_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,12 +17,12 @@ return new class extends Migration
             $table->decimal('tonnage')->unsigned()->unique();
         });
 
-        DB::table('tonnage')->insert(array('tonnage' => 1));
-        DB::table('tonnage')->insert(array('tonnage' => 1.5));
-        DB::table('tonnage')->insert(array('tonnage' => 3));
-        DB::table('tonnage')->insert(array('tonnage' => 5));
-        DB::table('tonnage')->insert(array('tonnage' => 10));
-        DB::table('tonnage')->insert(array('tonnage' => 20));
+        DB::table('tonnages')->insert(array('tonnage' => 1));
+        DB::table('tonnages')->insert(array('tonnage' => 1.5));
+        DB::table('tonnages')->insert(array('tonnage' => 3));
+        DB::table('tonnages')->insert(array('tonnage' => 5));
+        DB::table('tonnages')->insert(array('tonnage' => 10));
+        DB::table('tonnages')->insert(array('tonnage' => 20));
     }
 
     /**
