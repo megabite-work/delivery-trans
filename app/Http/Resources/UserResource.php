29ebb24a287281c146d88a3f2695052cca821edb
Yuri Zanin <y@zanin.me>
2024-08-11 14:49:22 +0300

    Проброс ролей в информацию о пользователе

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
             "id" => $this->id,
             "email" => $this->email,
             "name" => $this->name,
-            "roles" => $this->roles,
+            "roles" => RoleResource::collection($this->roles),
         ];
     }
 }
